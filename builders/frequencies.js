4a19e9fe9ed056b5e03845dbed43e803e501f16d
mstn <mstn@git.com>
2015-02-20 12:23:49 +0100

    generate only frequencies for included lines

--- builders/frequencies.js
+++ builders/frequencies.js
@@ -2,17 +2,22 @@ var _ = require('lodash');
 
 var mkTripId = require('../lib/utils').mkTripId;
 
-module.exports = function(data){
+module.exports = function(data, options){
+
+  options = options || {};
+  var include = options.include || [];
 
   var miccolis = data[0];
 
   var results = [];
 
-  miccolis.forEach(function( row ){
+  var includedLines = _.select(miccolis, function(row) { return _.contains(include, row.bus_number); });
+
+  includedLines.forEach(function( row ){
 
     var tripId = mkTripId( row );
 
-      results.push({
+    results.push({
       'trip_id': tripId,
       'start_time':row.start_time,
       'end_time':row.end_time,
